45c618dad07dfa0ba1d6f6aed1bec96b0ec7ca69
mohammad <mohamad.sanaye462@zoho.com>
2019-07-16 22:08:55 +0430

    Add config

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restplus import Resource, Api
+from Model.DataBase import db_session
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,3 +12,7 @@ class HelloWorld(Resource):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
