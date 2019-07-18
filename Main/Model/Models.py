2a689c9f30ecbd03f17d4f1870934a67f120b209
mohammad <mohamad.sanaye462@zoho.com>
2019-07-18 17:30:54 +0430

    Add service model

--- Main/Model/Models.py
+++ Main/Model/Models.py
@@ -12,6 +12,7 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True)
     passwordHash = db.Column(db.String(100))
     manager=db.relationship('Business', backref='user', lazy=True)
+    employee = db.relationship('Service', backref='user', lazy=True)
 
     @property
     def password(self):
@@ -36,4 +37,16 @@ class Business(db.Model):
     description = db.Column(db.String(255))
     managerID = db.Column(db.Integer, db.ForeignKey('user.id'),
         nullable=False)
-    
+    service = db.relationship('Service', backref='business', lazy=True)
+
+
+class Service(db.Model):
+    """ Service Model for storing service realated details """
+    __tablename__ = "service"
+
+    serviceID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255))
+    price = db.Column(db.Integer, nullable=False)
+    employerID = db.Column(db.Integer, db.ForeignKey('user.id'), nullable =False)
+    businessID = db.Column(db.Integer, db.ForeignKey('business.businessID'), nullable=False)
